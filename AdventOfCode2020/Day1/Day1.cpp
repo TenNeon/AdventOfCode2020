7edcdd0c0f4940435e5f1dc062d851b802e1c43d
TenNeon <TenNeon@users.noreply.github.com>
2020-12-12 18:37:27 -0800

    1B complete

--- AdventOfCode2020/Day1/Day1.cpp
+++ AdventOfCode2020/Day1/Day1.cpp
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include <vector>
-//#include <istream>
 #include <ostream> // ostream
 #include <fstream> // istream
 #include <string> // getline
@@ -84,6 +83,38 @@ int Do1A(std::vector<int> data)
   return 0;
 }
 
+int Do1B(std::vector<int> data)
+{
+  for (size_t i = 0; i < data.size()-1; i++)
+  {
+    for (size_t j = 1; j < data.size()-2; j++)
+    {
+      if (i != j)
+      {
+        int A = data[i];
+        int B = data[j];
+        int sumAB = A + B;
+        if (sumAB < 2020)
+        {
+          for (size_t k = 2; k < data.size() - 3; k++)
+          {
+            if (j != k)
+            {
+              int C = data[k];
+              int sum = A + B + C;
+              if (sum == 2020)
+              {
+                return A * B * C;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return 0;
+}
+
 int main()
 {
   std::string dataFileName = "Data/Data.txt";
@@ -98,6 +129,7 @@ int main()
   // Data to strings
   std::vector<std::string> lines_out;
   lines_out.push_back(std::to_string(Do1A(lines_int)));
+  lines_out.push_back(std::to_string(Do1B(lines_int)));
   //std::transform(lines_int.begin(), lines_int.end(), std::back_inserter(lines_out), [](int a) {return std::to_string(a); });
 
   WriteToFile(lines_out, outFileName);
